51a6800096d4cf7ce8bf5e93a6c725d924925ef9
Chenjunyu <mu_22mail@163.com>
2025-04-20 20:35:59 +0800

    起步阶段

---
+++ src/main/java/com/manage/librarydemo/LibraryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.manage.librarydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryDemoApplication.class, args);
+	}
+
+}
